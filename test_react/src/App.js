97a87ac5d50bc5be843153e2ef525820d99d50ac
yamaji-hironori <abc.3517.efg@gmail.com>
2023-10-05 00:29:30 +0900

    Fix: App.js, Ball.js

--- test_react/src/App.js
+++ test_react/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Ball from './components/Ball';
 
 const App = () => {
+  const [countState, setCountState] = useState(0);
+  const handleStateClick = () => setCountState(countState + 1);
+
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Ball />
-    </Canvas>
+    <div>
+      <button onClick={handleStateClick}>Change State: {countState}</button>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Ball count={countState} />
+      </Canvas>
+    </div>
   );
 };
 
--- test_react/src/components/Ball.js
+++ test_react/src/components/Ball.js
@@ -1,11 +1,14 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
-const Ball = () => {
-  const meshRef = useRef();
-  const trajectory = new THREE.CurvePath();
+const Ball = (props) => {
+  const { count } = props;
+  const meshRef = useRef(null);
+  const trajectory = useRef(new THREE.CurvePath()).current;
   const radius = 1;
+  const [countState, setCountState] = useState(0);
+  const handleStateClick = () => setCountState(countState + 1);
 
   useEffect(() => {
     const curve = new THREE.EllipseCurve(
@@ -17,19 +20,27 @@ const Ball = () => {
     );
 
     const points = curve.getPoints(50);
-    for (let i = 0; i < points.length; i++) {
+    // for (let i = 0; i < points.length; i++) {
+    //   const point = points[i];
+    //   trajectory.add(new THREE.LineCurve3(new THREE.Vector3(point.x, point.y, 0), new THREE.Vector3(point.x, point.y, 0)));
+    // }
+    for (let i = 0; i < points.length - 1; i++) {
       const point = points[i];
-      trajectory.add(new THREE.LineCurve3(new THREE.Vector3(point.x, point.y, 0), new THREE.Vector3(point.x, point.y, 0)));
+      const nextPoint = points[i + 1];
+      trajectory.add(new THREE.LineCurve3(new THREE.Vector3(point.x, point.y, 0), new THREE.Vector3(nextPoint.x, nextPoint.y, 0)));
     }
   }, []);
 
-  useFrame((state) => {
-    if (meshRef.current && trajectory) {
-      const time = state.clock.getElapsedTime();
-      const point = trajectory.getPointAt((time % 1));
-      meshRef.current.position.set(point.x, point.y, point.z);
-    }
-  });
+  useEffect(() => {
+    const point = trajectory.curves[count].v1;
+    console.log(point);
+    meshRef.current.position.set(point.x, point.y, point.z);
+  }, [count]);
+  // useFrame((state) => {
+  //   const time = state.clock.getElapsedTime();
+  //   const point = trajectory.getPointAt((time % 1));
+  //   meshRef.current.position.set(point.x, point.y, point.z);
+  // });
 
   return (
     <mesh ref={meshRef}>
@@ -40,3 +51,4 @@ const Ball = () => {
 };
 
 export default Ball;
+
